3c18a8c8e761a1f4cd2264e3e767811029fcda14
FarhanMJ <148459377+Frhnmj2004@users.noreply.github.com>
2024-07-28 00:57:22 +0530

    CreateFoodItem func completed

--- controllers/foodItemController.go
+++ controllers/foodItemController.go
@@ -1,6 +1,10 @@
 package controllers
 
 import (
+	"net/http"
+
+	"github.com/Frhnmj2004/restaurant-admin/helper"
+	"github.com/Frhnmj2004/restaurant-admin/models"
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
@@ -14,11 +18,59 @@ func NewFoodItemController(db *gorm.DB) *FoodItemController {
 }
 
 func (ctrl *FoodItemController) CreateFoodItem(ctx *fiber.Ctx) error {
+	type IngredientRequest struct {
+		GroceryName string  `json:"groceryname"`
+		Quantity    float64 `json:"quantity"`
+	}
+
+	type CreateFoodItemRequest struct {
+		Name        string              `json:"name"`
+		Price       float64             `json:"price"`
+		Ingredients []IngredientRequest `json:"ingredients"`
+	}
+
+	request := &CreateFoodItemRequest{}
+	err := ctx.BodyParser(request)
+	if err != nil {
+		return helper.ErrorResponse(ctx, http.StatusBadRequest, "Invalid request body")
+	}
+
+	ingredients := make([]models.Ingredient, len(request.Ingredients))
+	grocery := &models.Grocery{}
+
+	for _, ing := range request.Ingredients {
+		err := ctrl.DB.Where("name =?", ing.GroceryName).First(grocery).Error
+		if err != nil {
+			return helper.ErrorResponse(ctx, http.StatusNotFound, "Grocery not found :"+ing.GroceryName)
+		}
+
+		ingredients = append(ingredients, models.Ingredient{
+			GroceryID:   grocery.ID,
+			GroceryName: grocery.Name,
+			Quantity:    ing.Quantity,
+		})
+	}
+
+	foodItem := &models.FoodItem{
+		Name:        request.Name,
+		Price:       request.Price,
+		Ingredients: ingredients,
+	}
+
+	err = ctrl.DB.Create(foodItem).Error
+	if err != nil {
+		return helper.ErrorResponse(ctx, http.StatusInternalServerError, "Failed to create food item")
+	}
+
+	ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "Food item created successfully",
+		"data":    foodItem,
+	})
 
 	return nil
 }
 
-func (ctrl *FoodItemController) GetFoodItemByID(ctx *fiber.Ctx) error {
+func (ctrl *FoodItemController) GetFoodItemByName(ctx *fiber.Ctx) error {
 
 	return nil
 }
--- models/ingredient.go
+++ models/ingredient.go
@@ -4,9 +4,10 @@ import "github.com/jinzhu/gorm"
 
 type Ingredient struct {
 	gorm.Model
-	FoodItemID uint     `json:"fooditemid"`
-	GroceryID  uint     `json:"groceryid"`
-	Quantity   float64  `json:"quantity"`
-	Grocery    Grocery  `gorm:"foreignkey:GroceryID" json:"grocery"`
-	FoodItem   FoodItem `gorm:"foreignkey:FoodItemID" json:"fooditem"`
+	FoodItemID  uint     `json:"fooditemid"`
+	GroceryID   uint     `json:"groceryid"`
+	GroceryName string   `json:"groceryname"`
+	Quantity    float64  `json:"quantity"`
+	Grocery     Grocery  `gorm:"foreignkey:GroceryID" json:"grocery"`
+	FoodItem    FoodItem `gorm:"foreignkey:FoodItemID" json:"fooditem"`
 }
